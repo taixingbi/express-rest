6fdcc11d033f76e1d1059c0705c50dc46a7ebb69
taixingbi <bitaihang@gmail.com>
2018-12-10 21:18:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#### start    
+nodemon index.js     
+
+
+#### install epressjs  
+npm init      
+npm install --save express          
+npm install -g nodemon       
+
+npm install --save pug     
+npm install --save body-parser multer       
+
+#### install mongo
+npm install --save mongoose      
+//Start mongod server    
+mongod    
+//start mongo
+mongo
+
+#### install others
+npm install --save cookie-parser   
+
+
+
+
+
+
+   
---
+++ index.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+var app = express();
+var cookieParser = require('cookie-parser');
+var router = express.Router();
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
+
+//Require the Router we defined in movies.js
+var movies = require('./movies.js');
+
+//Use the Router on the sub route /movies
+app.use('/movies', movies);
+
+
+
+
+
+app.listen(3000);
